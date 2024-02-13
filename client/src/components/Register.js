d93295d858044e01c3e2ef8ca0612adbe50f5abe
Gnanigit <gnani4412@gmail.com>
2024-02-13 20:11:10 +0530

    file upload option

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import avatar from'../assets/profile.png';
 import styles from '../styles/Username.module.css';
 import { Toaster } from 'react-hot-toast';
 import { useFormik } from 'formik';
 import { passwordValidate } from '../helper/validate';
-
+import converToBase64 from '../helper/convert';
 
 
 export default function Register() {
+  const [file,setFile] = useState();
   const formik = useFormik({
     initialValues:{
       email:'demo@gmail.com',
@@ -19,9 +20,16 @@ export default function Register() {
     validateOnBlur:false,
     validateOnChange:false,
     onSubmit: async values =>{
+      values = await Object.assign(values,{profile:file || ""});
       console.log(values);
     }
   })
+
+  // formik does't support file upload so we need to create this handler
+  const onUpload = async e =>{
+    const base64 = await converToBase64(e.target.files[0]);
+    setFile(base64);
+  }
   return (
     <div className='container mx-auto'>
       <Toaster position='top-center' reverseOrder={false}></Toaster>
@@ -36,9 +44,9 @@ export default function Register() {
           <form className='py-1' onSubmit={formik.handleSubmit}>
           <div className='profile flex justify-center py-4'>
             <label htmlFor='profile'>
-              <img src={avatar} className={styles.profile_img} alt='avatar' />
+              <img src={file || avatar} className={styles.profile_img} alt='avatar' />
             </label>
-            <input type='file' id='profile' name='profile'/>
+            <input onChange={onUpload} type='file' id='profile' name='profile'/>
           </div>
 
             <div className='textbox flex flex-col items-center gap-6'>
