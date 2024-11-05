44befe64501ec97dac415fafbe2faccaf84244c9
dev1_clofast <gnani4412@gmail.com>
2024-11-05 13:12:45 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -5,42 +5,41 @@ import morgan from "morgan";
 import connect from "./database/conn.js";
 import router from "./router/route.js";
 
-
 // declaration and intialilzation
-const app=express();
-const port=8080
+const app = express();
+const port = 8080;
 
-const allowedOrigin = 'https://login-app-pink.vercel.app';
+const allowedOrigin = "https://login-app-pink.vercel.app";
 
 // uses
-app.use(express.json({ limit: '100mb' }));
-app.use(cors({
-    origin: allowedOrigin
-  }))
-app.use(morgan('tiny'))
-app.disable('x-powered-by')    // less hackers know about our stack
-
-
+app.use(express.json({ limit: "100mb" }));
+app.use(
+  cors({
+    origin: allowedOrigin,
+  })
+);
+// app.use(cors({}));
+app.use(morgan("tiny"));
+app.disable("x-powered-by"); // less hackers know about our stack
 
 // routes
-app.get('/',(req,res)=>{
-    res.status(201).json("Home GET Request");
-})
+app.get("/", (req, res) => {
+  res.status(201).json("Home GET Request");
+});
 
 // api routes - middleware
-app.use('/api',router)
+app.use("/api", router);
 
-
-
-connect().then(()=>{
-    try{
-        app.listen(port,(err)=>{
-            console.log(`Server connected to http://localhost:${port}`);
-        })
+connect()
+  .then(() => {
+    try {
+      app.listen(port, (err) => {
+        console.log(`Server connected to http://localhost:${port}`);
+      });
+    } catch (err) {
+      console.log("Cannot connect mongodb");
     }
-    catch(err){
-        console.log("Cannot connect mongodb");
-    }
-}).catch(error=>{
+  })
+  .catch((error) => {
     console.log("Invalid database connection");
-})
+  });
